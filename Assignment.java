ea45d07c807b74b85c872cb8879596b83198aecc
hariharanvastra <138756978+hariharanvastra@users.noreply.github.com>
2023-07-09 18:54:12 +0530

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -3,71 +3,64 @@ package practice;
 import java.util.Scanner;
 public class Assignment {
 	
-	Scanner user;
-	int add() {
-		user=new Scanner(System.in);
-		System.out.println("Enter two numbers for adding");
-		int num1=user.nextInt();
-	    int num2=user.nextInt();
-	    int sum=num1+num2;
-	   
-	    return sum;
-		
-	}
-    int subtract() {
-    	user=new Scanner(System.in);
-    	System.out.println("Enter two numbers for subtract");
-    	int num2=user.nextInt();
-	    int num3=user.nextInt();
-	    int sub=num2-num3;
-	 
-	    return sub;
-		
-	}
-     int multiple() {
-    	 user=new Scanner(System.in);
-    	 System.out.println("Enter two numbers for multiple");
-    	 int num4=user.nextInt();
- 	    int num5=user.nextInt();
- 	   int product=num4*num5;
-	  
-	    return product;
 	
+	int add(int num1,int num2) 
+	{
+
+	    int sum=num1+num2;	   
+	    return sum;		
+	}
+    int subtract(int num3,int num4) 
+	{
+	    int sub=num3-num4;	 
+	    return sub;		
+	}
+     int multiple(int num5,int num6)
+	{    	
+ 	   int product=num5*num6;	  
+	    return product;	
       }
-    int division() {
-    	user=new Scanner(System.in);
-    	System.out.println("Enter two numbers for division");
-    	int num6=user.nextInt();
-	    int num7=user.nextInt();
-	    int division=num6/num7;
-	  
-	    return division;
-	
+    int division(int num7,int num8) 
+	{
+	    int division=num7/num8;	  
+	    return division;	
      }
-    int power() {
-    	user=new Scanner(System.in);
-    	System.out.println("Enter a number for power");
-        int num8=user.nextInt();
-        int power=num8*num8;
-       
-        return power;
-        
-    
-	
+    int power(int num9) 
+	{  	
+        int power=num9*num9;       
+        return power;	
      }
 
 	public static void main(String[] args) {
 		Assignment obj=new Assignment();
-		
-		int addval=obj.add();
+		Scanner user=new Scanner(System.in);
+		System.out.println("Enter two numbers for adding");
+		int num1=user.nextInt();
+		int num2=user.nextInt();
+		int addval=obj.add(num1,num2);
 		System.out.println("your addition is :"+addval);
-		int subval=obj.subtract();
+
+		System.out.println("Enter two numbers for subtract");
+    	        int num2=user.nextInt();
+	        int num3=user.nextInt();		
+		int subval=obj.subtract(num2,num3);
 		System.out.println("your subtraction is :"+subval);
-		int mulval=obj.multiple();
+
+	        System.out.println("Enter two numbers for multiple");
+    	        int num4=user.nextInt();
+ 	        int num5=user.nextInt();
+		int mulval=obj.multiple(num4,num5);
 		System.out.println("your multiple is :"+mulval);
-		int divval=obj.division();
+
+		System.out.println("Enter two numbers for division");
+    	        int num6=user.nextInt();
+	        int num7=user.nextInt();
+		int divval=obj.division(num6,num7);
 		System.out.println("your division is :"+divval);
-		int powerval=obj.power();
+
+		System.out.println("Enter a number for power");
+                int num8=user.nextInt();
+		int powerval=obj.power(num8);
 		System.out.println("your power value is :"+powerval);
 		
 		
